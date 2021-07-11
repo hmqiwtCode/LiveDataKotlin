01e5e551260342b1e6f9624200e09362ed3916bc
Quy <huynhminhquy1999@gmail.com>
2021-07-11 23:45:51 +0700

    update live

--- app/src/main/java/com/quy/livedatakotlin/mediatore/MainActivity.kt
+++ app/src/main/java/com/quy/livedatakotlin/mediatore/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
 
+
         val model = ViewModelProviders.of(this)[MainViewModel::class.java]
 
 
